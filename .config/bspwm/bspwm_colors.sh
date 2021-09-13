4763092b72ac3aaec588c8c3f6b06814e7be14d6
Dylan Hanger <dylan.hanger@gmail.com>
2021-09-13 22:16:13 +0200

    added flavours

--- .config/bspwm/bspwm_colors.sh
+++ .config/bspwm/bspwm_colors.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
-
-bspc config focused_border_color "#d79921"
-bspc config normal_border_color  "#928374"
+#! /usr/bin/env bash
+bspc config normal_border_color 	\#1d1f21
+bspc config active_border_color 	\#1d1f21
+bspc config focused_border_color 	\#b294bb
+bspc config presel_feedback_color 	\#b294bb
+bspc config urgent_border_color 	\#cc6666
