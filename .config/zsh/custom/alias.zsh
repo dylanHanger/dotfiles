8aae7fceb6a75e37faa52fd7377a84ae843245f8
Dylan Hanger <dylan.hanger@gmail.com>
2023-05-05 10:15:01 +0200

    Forgot to commit some files

--- .config/zsh/custom/alias.zsh
+++ .config/zsh/custom/alias.zsh
@@ -1,4 +1,5 @@
 alias ls="exa --grid --icons --color=auto"
+alias lsa="exa --icons --color=auto -la"
 alias la="exa --grid --icons --color=auto -la"
 alias ll="exa --long --icons --color=auto"
 alias tree="exa --tree --icons --color=auto"
